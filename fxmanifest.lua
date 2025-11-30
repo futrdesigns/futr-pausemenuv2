2a6c87a7ea139ee132cc1af51565c9f38be013e7
futrdesigns <99038220+futrdesigns@users.noreply.github.com>
2025-11-30 18:38:38 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 description 'A nice advanced pausemenu'
 Author "futrdesigns"
-version '2.0.0'
+version '1.0.0'
 
 lua54 'yes'
 
@@ -21,6 +21,7 @@ files {
   'html/*',
   'html/images/*'
 }
+
 
 
 
