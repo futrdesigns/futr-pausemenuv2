a3bce0e283c2f8b2b8eb3d6cddffa91cc438e5fe
futrdesigns <99038220+futrdesigns@users.noreply.github.com>
2025-11-30 18:33:21 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.NativePauseMenuText = "FUTR Pause Menu v2"
+Config.ServerName = "FUTR Server"
+Config.ServerVersion = "v2.0.0"
---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'A nice advanced pausemenu'
+Author "futrdesigns"
+version '2.0.0'
+
+lua54 'yes'
+
+shared_scripts {
+    'config.lua',
+}
+
+client_scripts {
+    'client/main.lua',
+}
+
+ui_page 'html/index.html'
+
+files {
+  'html/*',
+  'html/images/*'
+}
+
+
+
